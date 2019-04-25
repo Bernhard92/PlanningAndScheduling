29ae5e306946e3aabec42adf2410b3bb72506131
bernhard <bernhard@isys.uni-klu.ac.at>
2019-04-25 13:33:09 +0200

    parse plan

--- src/OpenDoorsBot.java
+++ src/OpenDoorsBot.java
@@ -3,119 +3,95 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class OpenDoorsBot {
 
-	static int keyInput[] = { KeyEvent.VK_J, KeyEvent.VK_A, KeyEvent.VK_V, KeyEvent.VK_A, KeyEvent.VK_SPACE,
-			KeyEvent.VK_P, KeyEvent.VK_R, KeyEvent.VK_O, KeyEvent.VK_G, KeyEvent.VK_R, KeyEvent.VK_A, KeyEvent.VK_M,
-			KeyEvent.VK_M, KeyEvent.VK_I, KeyEvent.VK_N, KeyEvent.VK_G, KeyEvent.VK_SPACE, KeyEvent.VK_F, KeyEvent.VK_O,
-			KeyEvent.VK_R, KeyEvent.VK_U, KeyEvent.VK_M, KeyEvent.VK_S, KeyEvent.VK_SPACE, KeyEvent.VK_PERIOD,
-			KeyEvent.VK_C, KeyEvent.VK_O, KeyEvent.VK_M };
+	private Field position = null;
 
 	public static void main(String[] args) throws AWTException, IOException {
-		OpenDoorsBot instance = new OpenDoorsBot(); 
-		instance.parseInput(1);
+		OpenDoorsBot instance = new OpenDoorsBot();
+		// steps = instance.getStepsForLevel(1);
 
-		
+		Queue<Integer> steps = instance.getStepsForLevel(6);
+		instance.startWalking(steps);
 	}
-	
-	
-	private void startWalking() throws IOException, AWTException {
-		Runtime.getRuntime().exec(new String[]{"\"C:\\Program Files\\Mozilla Firefox\\firefox.exe\"", "https://armorgames.com/play/3357/open-doors-2?rt=r"});
-		//Runtime.getRuntime().exec("notepad");
+
+	private void startWalking(Queue<Integer> steps) throws IOException, AWTException {
+		// Runtime.getRuntime().exec(new String[] { "\"C:\\Program Files\\Mozilla
+		// Firefox\\firefox.exe\"",
+		// "https://armorgames.com/play/3357/open-doors-2?rt=r" });
 
 		Robot robot = new Robot();
+		robot.delay(5000);
 
-//		for (int i = 0; i < keyInput.length; i++) {
-//
-//			robot.keyPress(keyInput[i]);
-//			robot.delay(100);
-//
-//		}
-		
-		robot.delay(20000);
-		
-		goRight(robot);
-		goDown(robot);
-		goDown(robot);
-		goRight(robot);
-		goRight(robot);
-		goUp(robot);
-		goRight(robot);
-		goUp(robot);
-		goRight(robot);
-		goRight(robot);
-		goDown(robot);
-		goDown(robot);
-		goRight(robot);
-	}
-	
-	private void goRight(Robot robot) {
-		System.out.println("Going R");
-		robot.keyPress(KeyEvent.VK_RIGHT);
-		robot.delay(500);
-		robot.keyRelease(KeyEvent.VK_RIGHT);
-		robot.delay(1000);
-	}
-	
-	
-	private void goLeft(Robot robot) {
-		System.out.println("Going L");
-		robot.keyPress(KeyEvent.VK_LEFT);
-		robot.delay(500);
-		robot.keyRelease(KeyEvent.VK_LEFT);
-		robot.delay(1000);
-	}
-	
-	private void goUp(Robot robot) {
-		System.out.println("Going U");
-		robot.keyPress(KeyEvent.VK_UP);
-		robot.delay(500);
-		robot.keyRelease(KeyEvent.VK_UP);
-		robot.delay(1000);
+		for (int step : steps) {
+			robot.keyPress(step);
+			robot.delay(500);
+			robot.keyRelease(step);
+			robot.delay(500);
+		}
 	}
 
-	private void goDown(Robot robot) {
-		System.out.println("Going D");
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(1000);
-}
-	
-	private void parseInput(int planNumber) {
-		
-		File planFile = new File("./Plans/sas_plan"+planNumber+".txt");
+	private Queue<Integer> getStepsForLevel(int planNumber) {
+
+		File planFile = new File("./Plans/sas_plan" + planNumber + ".txt");
 		BufferedReader reader;
+		Queue<Integer> commandList = new LinkedList<>();
 		try {
 			reader = new BufferedReader(new FileReader(planFile));
-			List<KeyEvent> commandList = new ArrayList<>(); 
+
+			String command = "";
 			String from = "";
-			String to = ""; 
-			
-			String command = ""; 
+			String to = "";
+
 			while ((command = reader.readLine()) != null) {
-				if (command.contains("walk_free")) {
-					from = command.substring(12, 14); 
+
+				if (command.contains("walk_over_open_door")) {
+					from = command.substring(30, 32);
+					to = command.substring(26, 28);
+				} else if (command.contains("walk_over_door_angle_from_inside")) {
+					from = command.substring(39, 41);
+					to = command.substring(43, 45);
+				} else if (command.contains("walk_free") || command.contains("open_door")) {
+					from = command.substring(12, 14);
 					to = command.substring(16, 18);
 					
-					int x1 = Integer.parseInt(from.charAt(0));
-					int x2 = Integer.parseInt(from.charAt(1));
 				}
-				
-				System.out.print(from + " " + to);
+
+				System.out.println(from + " " + to);
+				commandList.add(calcDirection(from, to));
 			}
-		} catch (IOException e) {
+		} catch (
+
+		IOException e) {
 			System.out.println("Error while reading from file");
 			e.printStackTrace();
 		}
+		return commandList;
+	}
 
-		
+	private int calcDirection(String from, String to) {
+
+		Field fromField = new Field(Integer.parseInt(from.substring(0, 1)), Integer.parseInt(from.substring(1)));
+
+		Field toField = new Field(Integer.parseInt(to.substring(0, 1)), Integer.parseInt(to.substring(1)));
+
+		int direction;
+		if (fromField.x < toField.x) {
+			direction = KeyEvent.VK_RIGHT;
+		} else if (fromField.x > toField.x) {
+			direction = KeyEvent.VK_LEFT;
+		} else if (fromField.y < toField.y) {
+			direction = KeyEvent.VK_UP;
+		} else {
+			direction = KeyEvent.VK_DOWN;
+		}
+
+		return direction;
 	}
 
 }
